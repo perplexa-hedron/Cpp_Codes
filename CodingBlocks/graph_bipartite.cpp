9782db8e16688976ecde34f0e117e3d96358c920
Perplexa Hedron <piyushsinha3253@gmail.com>
2023-07-23 14:47:48 +0530

    more graphs

--- CodingBlocks/graph_bipartite.cpp
+++ CodingBlocks/graph_bipartite.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-class solution{
+class bfs_solution{
     private:
     bool check(int start,int V,vector<int> adj[],int color[]){
         queue<int> q;
@@ -32,6 +32,37 @@ class solution{
                 if(!check(i,V,adj,color)){
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+};
+
+class dfs_solution{
+    private:
+    bool check(int node,int col,vector<int> adj[],int color[]){
+        color[node] = col;
+        for(auto it: adj[node]){
+            if(color[it] == -1){
+                if(!check(it,!col,adj,color)){
+                    return false;
+                }
+            }
+            else if(color[it] == col){
+                return false;
+            }
+        }
+        return true;
+    }
+    public:
+    bool isBipartite(int V , vector<int> adj[]){
+        int color[V] ={-1};
+        for(int i =0;i<V;i++){
+            if(color[i] == -1){
+                if(!check(i,0,adj,color)){
+                    return false;
+                }
+
             }
         }
         return true;
